6bcbcaae45dad1b79ff9dd7ff717f542c2ab4e92
phares <abungaphares@gmail.com>
2016-03-12 11:01:17 +0300

    registration email sent fix

--- nims/settings.py
+++ nims/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER  = 'abungaphares@gmail.com'
+EMAIL_HOST_PASSWORD = 'champions2014/2015'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 # Application definition
 
