2c5bb0a13bf8bce749dca47a07f7db46666d4803
phares <abungaphares@gmail.com>
2016-04-11 11:52:52 +0300

    bulk urls modifiication

--- bulk/urls.py
+++ bulk/urls.py
@@ -4,14 +4,6 @@ from .import views
 urlpatterns = [
     url(r'^$', views.upload, name='upload'),
     url(r'^upload$', views.upload, name='upload'),
-    url(r'^review/$', views.review, name='review'),
-    url(r'^status/$', views.status, name='status'),
-    url(r'^create/$', views.create, name='create'),
     url(r'^account/$', views.account, name='account'),
-    url(r'^lipisha/$', views.lipisha_, name='lipisha'),
-    url(r'^test/$', views.test, name='test'),
-    url(r'^test/(?P<id>\d+)/$', views.test_detail, name='test_detail'),
-    url(r'^(?P<id>\d+)/edit/$', views.update, name='update'),
-    url(r'^(?P<id>\d+)/delete/$', views.delete, name='delete'),
 
 ]
