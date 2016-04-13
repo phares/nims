eecf58e0d8c7fe62c30502ff1e9d300d788b77ec
phares <abungaphares@gmail.com>
2016-04-13 08:23:13 +0300

    invalid params

--- bulk/views.py
+++ bulk/views.py
@@ -204,6 +204,7 @@ def status(request):
                             send_money = lipisha.send_money(account_number="05307", mobile_number=phone, amount=amount)
                             status = send_money['status']
                             messages.success(request, status['status_description'])
+                            messages.success(request, send_money)
 
                             payout['status'] = status['status']
 
